fcb8fb09ffa35e48d27b0a56850e94921da5eb63
larcohex <kudayar.pirimbaev@gmail.com>
2017-09-29 16:51:00 +0600

    * Added sitemap

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require ("express");
-const path = require('path');
+const path = require ("path");
 const app = express();
 const firebase = require ("firebase");
+const fs = require ("fs");
 
 function subjectName (subject) {
   switch (subject) {
@@ -48,6 +49,76 @@ app.listen (process.env.PORT || 8080);
 app.get ("/robots.txt", function (req, res) {
   res.type ("text/plain");
   res.sendFile (path.join (__dirname + "/robots.txt"));
+});
+
+app.get ("/sitemap.xml", function (req, res) {
+let sitemap = `
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://larcohex.herokuapp.com/</loc>
+    </url>
+    <url>
+      <loc>https://larcohex.herokuapp.com/olympiad</loc>
+    </url>
+    <url>
+      <loc>https://larcohex.herokuapp.com/olympiad?subject=0</loc>
+    </url>
+    <url>
+      <loc>https://larcohex.herokuapp.com/olympiad?subject=1</loc>
+    </url>
+    <url>
+      <loc>https://larcohex.herokuapp.com/olympiad?subject=2</loc>
+    </url>
+    <url>
+      <loc>https://larcohex.herokuapp.com/olympiad?subject=3</loc>
+    </url>
+    <url>
+      <loc>https://larcohex.herokuapp.com/olympiad?subject=4</loc>
+    </url>
+    <url>
+      <loc>https://larcohex.herokuapp.com/olympiad/math</loc>
+    </url>
+    <url>
+      <loc>https://larcohex.herokuapp.com/olympiad/physics</loc>
+    </url>
+    <url>
+      <loc>https://larcohex.herokuapp.com/olympiad/chemistry</loc>
+    </url>
+    <url>
+      <loc>https://larcohex.herokuapp.com/olympiad/biology</loc>
+    </url>
+    <url>
+      <loc>https://larcohex.herokuapp.com/olympiad/cs</loc>
+    </url>
+    <url>
+      <loc>https://larcohex.herokuapp.com/blog</loc>
+    </url>    
+`;
+  firebase.database().ref("/postrefs").once("value", function (snapshot) {
+    const val = snapshot.val();
+    for (let i = 0; i < Math.ceil (Object.keys (val).length / 3); ++i) {
+      sitemap += `
+        <url>
+          <loc>https://larcohex.herokuapp.com/blog?page=` + i + `</loc>
+        </url>
+      `;
+    }
+    for (let post in val) {
+      if (val.hasOwnProperty (post)) {
+        sitemap += `
+          <url>
+            <loc>https://larcohex.herokuapp.com/blog/` + val[post].ref + `</loc>
+          </url>
+        `;
+      }
+    }
+    sitemap += `
+      </urlset>
+    `;
+    res.set ("Content-Disposition", "attachment");
+    res.type ("text/xml");
+    res.send (sitemap);
+  });
 });
 
 app.get ("/olympiad/:subject", function (req, res) {
